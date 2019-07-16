578e93950d36f3b177050f4199dd1f8a16e0a8a3
Sebastian Lange <sebastian.lange@tum.de>
2019-07-17 00:38:19 +0200

    Cleaned up overlay script.

--- repository/Cohort_GenerateOverlay.R
+++ repository/Cohort_GenerateOverlay.R
@@ -13,15 +13,6 @@ repository_dir=args[1] #location of repository
 source(paste(repository_dir,"/Cohort_GenerateOverlayLibrary.R",sep=""))     
 
 Samples = list.dirs(full.name=F,recursive=F)
-#Samples=Samples[-grep("multiqc_data",Samples)]
-#Samples=Samples[-grep("REFERENCES",Samples)]
-#Samples=Samples[-grep("1N7T85",Samples)]
-#Samples=Samples[-grep("6P4EWP",Samples)]
-#Samples=Samples[-grep("J837KK_Exo",Samples)]
-#Samples=Samples[-grep("CohortDatabase",Samples)]
-#Samples=Samples[-grep("CNVplot_combined_by_chr",Samples)]
-#Samples=Samples[-grep("1_segment_annotated",Samples)]
-#Samples=Samples[-grep("misc",Samples)]
 
 for (SummaryStat in c("Mean","Proportion"))
 {
