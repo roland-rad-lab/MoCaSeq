a9320274ed1ad5f247089190d8aa849e95d05513
Sebastian Lange <sebastian.lange@tum.de>
2019-10-30 19:05:52 +0100

    Upated lcWGS analysis script

--- repository/all_MoCaSeq_lcWGS.sh
+++ repository/all_MoCaSeq_lcWGS.sh
@@ -432,7 +432,7 @@ echo '---- Plot CNV-profiles ----' | tee -a $name/results/QC/$name.report.txt
 echo -e "$(date) \t timestamp: $(date +%s)" | tee -a $name/results/QC/$name.report.txt
 
 Rscript $repository_dir/CNV_PlotCopywriter.R $name $species $repository_dir
-Rscript $repository_dir/CNV_MapSegmentsToGenes.R $name $species Copywriter 20000 $CGC_file $TruSight_file
+Rscript $repository_dir/CNV_MapSegmentsToGenes.R $name $species $genecode_file_genes Copywriter 20000 $CGC_file $TruSight_file
 sh $repository_dir/CNV_CleanUp.sh $name
 
 echo '---- Run HMMCopy (bin-size 20000) ----' | tee -a $name/results/QC/$name.report.txt
@@ -445,7 +445,7 @@ echo -e "$(date) \t timestamp: $(date +%s)" | tee -a $name/results/QC/$name.repo
 
 Rscript $repository_dir/CNV_PlotHMMCopy.R $name $species $repository_dir $sequencing_type 20000 \
 $mapWig_file $gcWig_file $centromere_file $varregions_file
-Rscript $repository_dir/CNV_MapSegmentsToGenes.R $name $species HMMCopy 20000 $CGC_file $TruSight_file
+Rscript $repository_dir/CNV_MapSegmentsToGenes.R $name $species $genecode_file_genes HMMCopy 20000 $CGC_file $TruSight_file
 
 echo '---- Run HMMCopy (bin-size 1000) ----' | tee -a $name/results/QC/$name.report.txt
 echo -e "$(date) \t timestamp: $(date +%s)" | tee -a $name/results/QC/$name.report.txt
