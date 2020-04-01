df3e2162fed34a67b2016f6a83e97eb27c7f9497
Sebastian Lange <sebastian.lange@tum.de>
2020-04-02 01:30:08 +0200

    Update Cohort_GenerateOverlay.R

--- repository/Cohort_GenerateOverlay.R
+++ repository/Cohort_GenerateOverlay.R
@@ -13,12 +13,16 @@ repository_dir=args[1] #location of repository
 source(paste(repository_dir,"/Cohort_GenerateOverlayLibrary.R",sep=""))     
 
 Samples = list.dirs(full.name=F,recursive=F)
+Method="HMMCopy"
+species="Mouse"
+Paths=rep("",length(Samples))
+Save_path=""
 
 for (SummaryStat in c("Mean","Proportion"))
 {
   for (Ylim in c(2,5))
   {
-    RunOverlayAnalysis(Samples=Samples,Method="HMMCopy",species="Mouse",resolution=20000,SummaryStat=SummaryStat,AberrationCutoff=0.25,ChromomsomesToRemove="",Ylim=Ylim,format="pdf",Suffix=paste0(".",SummaryStat,".",Ylim))
+    RunOverlayAnalysis(Samples=Samples,Paths=Paths,Method=Method,species=species,resolution=20000,SummaryStat=SummaryStat,AberrationCutoff=0.25,ChromomsomesToRemove="",Ylim=Ylim,format="pdf",Suffix=paste0(".",SummaryStat,".",Ylim),Save_path=Save_path)
   }
 }
 
