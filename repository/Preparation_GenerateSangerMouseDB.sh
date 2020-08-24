0ca6baf8d5217b1bc59df91a86cfd2e2a09ca4d0
Sebastian Lange <sebastian.lange@tum.de>
2020-08-24 22:36:45 +0200

    Fixes a bug introduced in v0.4.47 which resulted in an empty SangerMouse database for SNPs and Indels.

--- repository/Preparation_GenerateSangerMouseDB.sh
+++ repository/Preparation_GenerateSangerMouseDB.sh
@@ -37,9 +37,9 @@ cwd=$(pwd)
 #changing directories here, so that the input order is similar between both commands
 cd $temp_dir/ftp-mouse.sanger.ac.uk/REL-1505-SNPs_Indels/strain_specific_vcfs/
 
-bcftools merge *.snps.dbSNP142.vcf.gz.filter.sort -m none -o "$temp_dir"/Merged.mgp.v5.snps.dbSNP142.filter.vcf.gz -O z --temp-dir ${temp_dir}
+bcftools merge *.snps.dbSNP142.vcf.gz.filter.sort -m none -o "$temp_dir"/Merged.mgp.v5.snps.dbSNP142.filter.vcf.gz -O z
 
-bcftools merge *.indels.dbSNP142.normed.vcf.gz.filter.sort -m none -o "$temp_dir"/Merged.mgp.v5.indels.dbSNP142.filter.vcf.gz -O z --temp-dir ${temp_dir}
+bcftools merge *.indels.dbSNP142.normed.vcf.gz.filter.sort -m none -o "$temp_dir"/Merged.mgp.v5.indels.dbSNP142.filter.vcf.gz -O z
 
 cd $cwd
 
@@ -47,7 +47,7 @@ tabix -p vcf "$temp_dir"/Merged.mgp.v5.snps.dbSNP142.filter.vcf.gz
 
 tabix -p vcf "$temp_dir"/Merged.mgp.v5.indels.dbSNP142.filter.vcf.gz
 
-bcftools concat -a -O z -o $temp_dir/MGP.v5.snp_and_indels.exclude_wild.vcf.gz $temp_dir/Merged.mgp.v5.snps.dbSNP142.filter.vcf.gz $temp_dir/Merged.mgp.v5.indels.dbSNP142.filter.vcf.gz --temp-dir ${temp_dir}
+bcftools concat -a -O z -o $temp_dir/MGP.v5.snp_and_indels.exclude_wild.vcf.gz $temp_dir/Merged.mgp.v5.snps.dbSNP142.filter.vcf.gz $temp_dir/Merged.mgp.v5.indels.dbSNP142.filter.vcf.gz
 
 bcftools sort -O z -o ref/"$VersionMouse"/MGP.v5.snp_and_indels.exclude_wild.vcf.gz $temp_dir/MGP.v5.snp_and_indels.exclude_wild.vcf.gz --temp-dir ${temp_dir}
 
