6b115845194d16a686d16743f4c5f767393ccc51
Niklas dAK <niklas.andrade-kraetzig@tum.de>
2020-10-13 14:23:59 +0200

    added check for empty files

--- repository/CheckReferenceFiles.sh
+++ repository/CheckReferenceFiles.sh
@@ -8,17 +8,25 @@
 ##
 ##########################################################################################
 
-FileList=$@ # takes all variables from the input list 
+FileList=$@ # takes all variables from the input list
 
+# set binary flag
 allfound=yes
+
 for file in $FileList; do
   if [ ! -f $file ]; then
+  # check if file is found
   echo "File not found: $file"
+  allfound=no
+  elif [ ! -s $file ]; then
+  # check if file is not empty
+  echo "File is empty: $file"
   allfound=no
   fi
 done
 
+
 if [ $allfound == "no" ]; then
-  echo "Some mandatory files are missing! Please check the consistency of your genome reference folder or and check for errors during the genome building process"
+  echo "Some mandatory files have issues! Please check the consistency of your genome reference folder or and check for errors during the genome building process"
   exit 1
 fi
