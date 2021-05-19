012bd4f0f7d6762940d920994ed2a3ff2c5a119b
sc13.bioinf <sc13.bioinf@googlemail.com>
2021-05-19 18:04:34 +0200

    ...

--- repository/SV_library.R
+++ repository/SV_library.R
@@ -47,3 +47,41 @@ sv_from_manta <- function (data)
 	return (result)
 }
 
+sv_from_delly <- function (data)
+{
+	delly_data_aux <- data %>%
+		filter (alt!="<INS>") %>%
+		mutate (info.end=as.numeric (info.end)) %>%
+		mutate (id_sample=paste (id,sample,sep="__")) %>%
+		data.frame
+
+	deletions <- delly_data_aux %>%
+		filter (info.svtype=="DEL") %>%
+		mutate (chrom1=chrom,pos1=pos,chrom2=chrom,pos2=info.end,SVtype=info.svtype,strand1="+",strand2="-") %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		data.frame
+
+	duplications <- delly_data_aux %>%
+		filter (info.svtype=="DUP") %>%
+		mutate (chrom1=chrom,pos1=pos,chrom2=chrom,pos2=info.end,SVtype=info.svtype,strand1="-",strand2="+") %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		data.frame
+
+	inversion_head_to_head <- delly_data_aux %>%
+		filter (info.svtype=="BND",stringr::str_detect (alt,"^\\]|^\\[")) %>%
+		mutate (chrom1=chrom,pos1=pos,chrom2=info.chr2,pos2=info.pos2,SVtype="h2hINV",strand1="+",strand2="+") %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		data.frame
+
+	inversion_tail_to_tail <- delly_data_aux %>%
+		filter (info.svtype=="BND",stringr::str_detect (alt,"\\]$|\\[$")) %>%
+		mutate (chrom1=chrom,pos1=pos,chrom2=info.chr2,pos2=info.pos2,SVtype="t2tINV",strand1="-",strand2="-") %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		data.frame
+
+	result <- rbind (deletions,duplications,inversion_head_to_head,inversion_tail_to_tail)
+
+	return (result)
+}
+
+
