013bab0f0a86290fdd6e23f68c550f1ebe7f4589
sc13.bioinf <sc13.bioinf@googlemail.com>
2021-05-20 18:40:23 +0200

    wip, need to fix/remove use of group_indices

--- modules/local/software/structural-variation/main.nf
+++ modules/local/software/structural-variation/main.nf
@@ -18,8 +18,8 @@ process structural_variation_matched {
 echo -e "SAMPLE\\tCHROM\\tPOS\\tID\\tREF\\tALT\\tINFO.IMPRECISE\\tINFO.SVTYPE\\tINFO.SVLEN\\tINFO.END\\tINFO.CIPOS\\tINFO.CIEND\\tINFO.CIGAR\\tINFO.MATEID\\tINFO.EVENT\\tINFO.HOMLEN\\tINFO.HOMSEQ\\tINFO.SVINSLEN\\tINFO.SVINSSEQ\\tINFO.LEFT_SVINSSEQ\\tINFO.RIGHT_SVINSSEQ\\tINFO.BND_DEPTH\\tINFO.MATE_BND_DEPTH\\tINFO.SOMATIC\\tINFO.SOMATICSCORE\\tINFO.JUNCTION_SOMATICSCORE\\tPR\\tSR" | tr "[A-Z]" "[a-z]" > ${meta.sampleName}.manta.tsv
 bcftools view -f PASS ${manta_vcf} | bcftools query -f '[%SAMPLE\\t%CHROM\\t%POS\\t%ID\\t%REF\\t%ALT\\t%INFO/IMPRECISE\\t%INFO/SVTYPE\\t%INFO/SVLEN\\t%INFO/END\\t%INFO/CIPOS\\t%INFO/CIEND\\t%INFO/CIGAR\\t%INFO/MATEID\\t%INFO/EVENT\\t%INFO/HOMLEN\\t%INFO/HOMSEQ\\t%INFO/SVINSLEN\\t%INFO/SVINSSEQ\\t%INFO/LEFT_SVINSSEQ\\t%INFO/RIGHT_SVINSSEQ\\t%INFO/BND_DEPTH\\t%INFO/MATE_BND_DEPTH\\t%INFO/SOMATIC\\t%INFO/SOMATICSCORE\\t%INFO/JUNCTION_SOMATICSCORE\\t%PR\\t%SR\n]' >> ${meta.sampleName}.manta.tsv
 
-echo -e "SAMPLE\\tCHROM\\tPOS\\tID\\tREF\\tALT\\tINFO.IMPRECISE\\tINFO.SVTYPE\\tINFO.SVLEN\\tINFO.END\\tINFO.CIPOS\\tINFO.CIEND\\tINFO.CHR2\\tINFO.POS2\\tRR\\tRV" | tr "[A-Z]" "[a-z]" > ${meta.sampleName}.delly.tsv
-bcftools view -f PASS ${delly_bcf} | bcftools query -f '[%SAMPLE\\t%CHROM\\t%POS\\t%ID\\t%REF\\t%ALT\\t%INFO/IMPRECISE\\t%INFO/SVTYPE\\t%INFO/SVLEN\\t%INFO/END\\t%INFO/CIPOS\\t%INFO/CIEND\\t%INFO/CHR2\\t%INFO/POS2\\t%RR\\t%RV\n]' >> ${meta.sampleName}.delly.tsv
+echo -e "SAMPLE\\tCHROM\\tPOS\\tID\\tREF\\tALT\\tINFO.IMPRECISE\\tINFO.SVTYPE\\tINFO.SVLEN\\tINFO.END\\tINFO.CIPOS\\tINFO.CIEND\\tINFO.CHR2\\tINFO.POS2\\tRR\\tRV\\tGT\\tGQ\\tFT" | tr "[A-Z]" "[a-z]" > ${meta.sampleName}.delly.tsv
+bcftools view -f PASS ${delly_bcf} | bcftools query -f '[%SAMPLE\\t%CHROM\\t%POS\\t%ID\\t%REF\\t%ALT\\t%INFO/IMPRECISE\\t%INFO/SVTYPE\\t%INFO/SVLEN\\t%INFO/END\\t%INFO/CIPOS\\t%INFO/CIEND\\t%INFO/CHR2\\t%INFO/POS2\\t%RR\\t%RV\\t%GT\\t%GQ\\t%FT\n]' >> ${meta.sampleName}.delly.tsv
 
 echo -e "sample\tchrom\tstart\tend\tlog2" > ${meta.sampleName}.cnvkit.tsv
 cat ${normal_cns} | sed -e '1d;' | awk BEGIN'{FS=OFS="\\t";}{print "Normal",\$1,\$2,\$3,\$5;}' >> ${meta.sampleName}.cnvkit.tsv
@@ -39,6 +39,7 @@ process structural_variation_matched_merge {
 	"""#!/usr/bin/env Rscript
 library (dplyr)
 library (stringr)
+library (IRanges)
 
 source ("${params.script_base}/SV_library.R")
 
@@ -59,7 +60,42 @@ head (data_delly_sv)
 
 data_sv <- rbind (data_manta_sv %>% mutate(caller="manta") %>% data.frame,data_delly_sv %>% mutate(caller="delly") %>% data.frame)
 
-write.table (data_sv,file="sv.combined.tsv",sep="\\t",row.names=F,quote=F)
+print ("data_sv")
+print (head(data_sv))
+print (class(data_sv[,"pos1"]))
+
+# https://www.bioconductor.org/packages/release/bioc/html/StructuralVariantAnnotation.html
+# ^ Can read from the VCFs and do the merging, plus output to bedpe and circos plots
+# at some point borrow more of their merge ideas (i.e. CI)
+# https://github.com/PapenfussLab/StructuralVariantAnnotation
+
+data_sv_merged <- data_sv %>%
+	#filter (sample=="Tumor",SVtype=="t2tINV",chrom1=="3",chrom2=="1") %>%
+	#filter (sample=="Normal",SVtype=="DUP",chrom1=="14",chrom2=="14") %>%
+	#bind_cols (event_id_sample_sv_chrom=group_indices (.,sample,SVtype,chrom1,chrom2)) %>%
+	#mutate (event_id_sample_sv_chrom=group_by (.,sample,SVtype,chrom1,chrom2) %>% group_indices ()) %>%
+	group_by (sample,SVtype,chrom1,chrom2) %>%
+	mutate(event_id_sample_sv_chrom=group()) %>%
+	group_modify (sv_find_event) %>%
+	ungroup () %>%
+	dplyr::select (-c(region1,region2)) %>%
+	dplyr::rename (event_id_sample_sv_chrom_event=event_id) %>%
+	bind_cols (event_id_sample=group_indices (.,event_id_sample_sv_chrom,event_id_sample_sv_chrom_event)) %>%
+	bind_cols (event_id_sv_chrom=group_indices (.,SVtype,chrom1,chrom2)) %>%
+	group_by (SVtype,chrom1,chrom2) %>%
+	group_modify (sv_find_event) %>%
+	ungroup () %>%
+	dplyr::select (-c(region1,region2)) %>%
+	dplyr::rename (event_id_sv_chrom_event=event_id) %>%
+	bind_cols (event_id=group_indices (.,event_id_sv_chrom,event_id_sv_chrom_event)) %>%
+	group_by (event_id) %>%
+	#group_modify(function (data,group) { print (data[,"sample"]);print ("Normal" %in% data[,"sample"]); cbind (data,somatic="Normal" %in% data[,"sample"]) }) %>%
+	mutate (somatic=match("Normal",sample,nomatch=0)) %>%
+	ungroup () %>%
+	dplyr::select (-c (event_id_sample_sv_chrom,event_id_sample_sv_chrom_event,event_id_sv_chrom,event_id_sv_chrom_event)) %>%
+	data.frame
+
+write.table (data_sv_merged,file="${meta.sampleName}.sv.events.tsv",sep="\\t",row.names=F,quote=F)
 
 	"""
 }
--- repository/SV_library.R
+++ repository/SV_library.R
@@ -1,11 +1,13 @@
 #!/usr/bin/env Rscript
 
-
+# The manta VCF is somatic variants only
 sv_from_manta <- function (data)
 {
 	manta_data_aux <- data %>%
+		bind_cols (setNames(as.data.frame(apply(stringr::str_split_fixed (.$pr,",",2),2,as.numeric),stringsAsFactors=F),c("pr.ref","pr.alt"))) %>%
+		bind_cols (setNames(as.data.frame(apply(stringr::str_split_fixed (.$sr,",",2),2,as.numeric),stringsAsFactors=F),c("sr.ref","sr.alt"))) %>%
 		filter (alt!="<INS>") %>%
-		mutate (info.end=as.numeric (info.end)) %>%
+		mutate (info.end=as.numeric (info.end),info.svlen=as.numeric (info.svlen)) %>%
 		mutate (id_sample=paste (id,sample,sep="__"),info.mateid_sample=paste (info.mateid,sample,sep="__"),info.event_sample=paste (info.event,sample,sep="__")) %>%
 		data.frame
 
@@ -14,13 +16,21 @@ sv_from_manta <- function (data)
 	deletions <- manta_data_aux %>%
 		filter (info.svtype=="DEL") %>%
 		mutate (chrom1=chrom,pos1=pos,chrom2=chrom,pos2=info.end,SVtype=info.svtype,strand1="+",strand2="-") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		mutate (gt=case_when (
+				      pr.ref + sr.ref == 0 ~ "1/1",
+				      TRUE ~ "0/1"
+				      )) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	duplications <- manta_data_aux %>%
 		filter (info.svtype=="DUP") %>%
 		mutate (chrom1=chrom,pos1=pos,chrom2=chrom,pos2=info.end,SVtype=info.svtype,strand1="-",strand2="+") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		mutate (gt=case_when (
+				      pr.ref + sr.ref == 0 ~ "1/1",
+				      TRUE ~ "0/1"
+				      )) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	# Inversions are described in terms of breakends (type BND) according to the VCF 4.2 spec
@@ -32,14 +42,23 @@ sv_from_manta <- function (data)
 		filter (info.svtype=="BND",stringr::str_detect (alt,"^\\]|^\\[")) %>%
 		inner_join (manta_data_aux,by=c("id_sample"="info.mateid_sample"),suffix=c("1","2")) %>%
 		mutate (sample=sample1,SVtype="h2hINV",strand1="+",strand2="+") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		mutate (gt=case_when (
+				      pr.ref1 + pr.ref2 + sr.ref1 + sr.ref2 == 0 ~ "1/1",
+				      TRUE ~ "0/1"
+				      )) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	inversion_tail_to_tail <- manta_data_aux %>%
 		filter (info.svtype=="BND",stringr::str_detect (alt,"\\]$|\\[$")) %>%
 		inner_join (manta_data_aux,by=c("id_sample"="info.mateid_sample"),suffix=c("1","2")) %>%
 		mutate (sample=sample1,SVtype="t2tINV",strand1="-",strand2="-") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+mutate (gt=case_when (
+				      pr.ref1 + pr.ref2 + sr.ref1 + sr.ref2 == 0 ~ "1/1",
+				      TRUE ~ "0/1"
+				      )) %>%
+
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	result <- rbind (deletions,duplications,inversion_head_to_head,inversion_tail_to_tail)
@@ -50,33 +69,34 @@ sv_from_manta <- function (data)
 sv_from_delly <- function (data)
 {
 	delly_data_aux <- data %>%
+		#filter (ft=="PASS") %>%# The FILTER is PASS but FORMAT/FT is LowQual for all samples??
 		filter (alt!="<INS>") %>%
-		mutate (info.end=as.numeric (info.end)) %>%
+		mutate (info.end=as.numeric (info.end),info.svlen=as.numeric (info.svlen),info.pos2=as.numeric (info.pos2)) %>%
 		mutate (id_sample=paste (id,sample,sep="__")) %>%
 		data.frame
 
 	deletions <- delly_data_aux %>%
 		filter (info.svtype=="DEL") %>%
 		mutate (chrom1=chrom,pos1=pos,chrom2=chrom,pos2=info.end,SVtype=info.svtype,strand1="+",strand2="-") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	duplications <- delly_data_aux %>%
 		filter (info.svtype=="DUP") %>%
 		mutate (chrom1=chrom,pos1=pos,chrom2=chrom,pos2=info.end,SVtype=info.svtype,strand1="-",strand2="+") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	inversion_head_to_head <- delly_data_aux %>%
 		filter (info.svtype=="BND",stringr::str_detect (alt,"^\\]|^\\[")) %>%
 		mutate (chrom1=chrom,pos1=pos,chrom2=info.chr2,pos2=info.pos2,SVtype="h2hINV",strand1="+",strand2="+") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	inversion_tail_to_tail <- delly_data_aux %>%
 		filter (info.svtype=="BND",stringr::str_detect (alt,"\\]$|\\[$")) %>%
 		mutate (chrom1=chrom,pos1=pos,chrom2=info.chr2,pos2=info.pos2,SVtype="t2tINV",strand1="-",strand2="-") %>%
-		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2) %>%
+		dplyr::select (sample,chrom1,pos1,chrom2,pos2,SVtype,strand1,strand2,gt) %>%
 		data.frame
 
 	result <- rbind (deletions,duplications,inversion_head_to_head,inversion_tail_to_tail)
@@ -84,4 +104,32 @@ sv_from_delly <- function (data)
 	return (result)
 }
 
+sv_find_event <- function (data, group)
+{
+	#print ("sv_find_event:")
+	#print (data)
+	#print (group)
+
+	flank <- 50
+
+	pos1r <- IRanges (start=data %>% pull (pos1),width=1)#IRanges auto merges unless width/end is specified
+	pos1rem <- IRanges::reduce (IRanges::resize (pos1r,width=flank*2,fix="center"))
+
+	pos2r <- IRanges (start=data %>% pull (pos2),width=1)#IRanges auto merges unless width/end is specified
+	pos2rem <- IRanges::reduce (IRanges::resize (pos2r,width=flank*2,fix="center"))
+
+	# findOverlaps (query, subject
+	pos1r_in_pos1rem <- IRanges::findOverlaps (pos1r,pos1rem,type="within",select="all")
+	pos2r_in_pos2rem <- IRanges::findOverlaps (pos2r,pos2rem,type="within",select="all")
+
+	result <- data %>%
+		mutate (region1=!!S4Vectors::subjectHits (pos1r_in_pos1rem)) %>%
+		mutate (region2=!!S4Vectors::subjectHits (pos2r_in_pos2rem)) %>%
+		#bind_cols (event_id=group_indices (.,region1,region2)) %>%
+		group_by (region1,region2) %>%
+		mutate (event_id=cur_group_id ()) %>%
+		data.frame
+
+	return (result)
+}
 
