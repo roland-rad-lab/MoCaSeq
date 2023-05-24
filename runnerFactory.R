304c72297f0f5d53d69e5f2cd03a4ab55646ddf7
Marcus Wagner <marcus.wagner@tum.de>
2023-05-24 15:45:34 +0200

    updated runnerFactory.R with shared work dir and shared MoCaSeq repo path

--- runnerFactory.R
+++ runnerFactory.R
@@ -1,7 +1,12 @@
 #!/usr/bin/env Rscript
 
-# script to automate input.tsv table creation for mocaseq pipeline run
+# Script to automate launch script and input.tsv table creation for MoCaSeq pipeline
+# see https://github.com/roland-rad-lab/MoCaSeq/tree/human-pipeline-nextflow-2
 # @author: marcus.wagner@tum.de
+# 
+# This script generates runner scripts and input tables for MoCaSeq runs on LRZ
+# Please make sure all directories you want to use are valid and you have permissions.
+# 
 
 # example content of input.tsv 
 # Sample_Name	Sample_Group	Library_ID	Lane	Colour_Chemistry	SeqType	Organism	Type	R1	R2	BAM
@@ -13,6 +18,25 @@ library(dplyr)
 library(tidyr)
 library(splitstackshape)
 
+# MoCaSeq call parameters
+# make sure these directories exist or adapt these values to your need
+project_dir <- '/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS'
+work_dir <- '/gpfs/scratch/pn29ya/ga89tog2/mocaseq-nextflow' # add [remap|mocaseq]/work later
+
+ega_data_path <- '/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh37_bam'
+genome_build.human <- 'GRCh38.p12'
+custom_config_version <- 'mocaseq-lrz'
+custom_config_base <- file.path(project_dir, 'MoCaSeq', 'conf')
+input_prefix <- file.path(project_dir, 'MoCaSeq', 'input')
+
+remap_out_base <- file.path(project_dir, 'input')
+mocaseq_out_base <- file.path(project_dir, 'output')
+
+# batch01 test samples for benchmarking to previous work
+# batch01_test <- c("RAMP_0007_Lv_M_5262", "RAMP_0007_Mu_R", "RAMP_0008_Ln_M_526",
+#                   "RAMP_0008_Lv_M_526", "RAMP_0008_Mu_R", "RAMP_0008_Pa_P")
+
+# read sample info table
 dt.info <- fread('../../AllCompassAssociatedEgadFiles_2022-02-24.csv')
 dt.info[Downloaded == 'corrupted-2021-12', Downloaded := 'corrupted_2021-12']
 # separate batch
@@ -25,23 +49,6 @@ dt.info[grep('yes', Date), EGAonLRZ := T]
 
 # split yes/no from Date
 dt.info[, Date := substr(Date, 1, 7)]
-
-# MoCaSeq call parameters
-# make sure these directories exist in your $USER paths
-ega_data_path <- '/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh37_bam'
-project_dir <- '/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS'
-work_dir <- '/gpfs/scratch/pn29ya/${USER}/${USER}/mocaseq-nf' # add [remap|mocaseq]/work later
-genome_build.human <- 'GRCh38.p12'
-custom_config_version <- 'mocaseq-lrz'
-custom_config_base <- 'https://raw.githubusercontent.com/roland-rad-lab/MoCaSeq/human-pipeline-nextflow-2/conf'
-input_prefix <- '/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS/mocaseq_input_tables'
-
-remap_out_base <- file.path(project_dir, 'input')
-mocaseq_out_base <- file.path(project_dir, 'output')
-
-# batch01 test samples
-# batch01_test <- c("RAMP_0007_Lv_M_5262", "RAMP_0007_Mu_R", "RAMP_0008_Ln_M_526",
-#                   "RAMP_0008_Lv_M_526", "RAMP_0008_Mu_R", "RAMP_0008_Pa_P")
 
 # get only samples that are currently present on LRZ
 # alternatively filter for samples here
@@ -79,20 +86,23 @@ for (s_group in sample_groups) {
                          Colour_Chemistry, SeqType, Organism, Type, R1, R2, BAM)],
             file = file.path('input', 'remap', paste0(s_group, '.tsv')),
             sep = '\t', row.names = F, quote = F)
-
+  
   # write caller script file by Sample_Group
-  cat(paste0('nextflow run roland-rad-lab/MoCaSeq -r human-pipeline-nextflow-2 ',
+  cat(paste0('# MoCaSeq remapping launch script for sample group', s_group, '\n'),
+      paste0('projectDir=', project_dir, '\n'),
+      paste0('workDir=', work_dir, '\n'),
+      paste0('nextflow run ', file.path('${projectDir}', 'MoCaSeq', 'main.nf'),
              ' -profile charliecloud,slurm', ' -entry MAP',
-             ' -work-dir ', file.path(work_dir, 'remap', 'work'),
-             ' --output_base ', remap_out_base,
+             ' -work-dir ', file.path('${workDir}', 'remap', 'work'),
+             ' --output_base ', file.path('${projectDir}', 'input'),
              ' --genome_build.human ', genome_build.human,
              ' --custom_config_version ', custom_config_version,
-             ' --custom_config_base ', custom_config_base,
-             ' --input ', input_prefix, 'remap/', s_group, '.tsv'),
+             ' --custom_config_base ', file.path('${projectDir}', 'MoCaSeq', 'conf'),
+             ' --input ', file.path('${projectDir}', 'MoCaSeq', 'input', 'remap', s_group, '.tsv')),
       file = file.path('launch', 'remap', paste0(s_group, '_remap.sh')))
   
   # make runner file executable for user
-  system(paste0("chmod u+x ",
+  system(paste0("chmod ug+x ",
                 file.path('launch', 'remap', paste0(s_group, '_remap.sh'))))
 }
 
@@ -115,17 +125,20 @@ for (s_group in sample_groups) {
             sep = '\t', row.names = F, quote = F)
   
   # write caller script file by Sample_Group
-  cat(paste0('nextflow run roland-rad-lab/MoCaSeq -r human-pipeline-nextflow-2 ',
+  cat(paste0('# MoCaSeq launch script for sample group', s_group, '\n'),
+      paste0('projectDir=', project_dir, '\n'),
+      paste0('workDir=', work_dir, '\n'),
+      paste0('nextflow run ', file.path('${projectDir}', 'MoCaSeq', 'main.nf'),
              ' -profile charliecloud,slurm',
-             ' -work-dir ', file.path(work_dir, 'mocaseq', 'work'), 
-             ' --output_base ', mocaseq_out_base, 
+             ' -work-dir ', file.path('${workDir}', 'mocaseq', 'work'), 
+             ' --output_base ', file.path('${projectDir}', 'output'), 
              ' --genome_build.human ', genome_build.human,
              ' --custom_config_version ', custom_config_version,
-             ' --custom_config_base ', custom_config_base, 
-             ' --input ', input_prefix, 'mocaseq/', paste0(s_group, '.tsv')),
+             ' --custom_config_base ', file.path('${projectDir}', 'MoCaSeq', 'conf'), 
+             ' --input ', file.path('${projectDir}', 'MoCaSeq', 'input', 'mocaseq', s_group, '.tsv')),
       file = file.path('launch', 'mocaseq', paste0(s_group, '_mocaseq.sh')))
   
   # make runner file executable for user
-  system(paste0("chmod u+x ", 
+  system(paste0("chmod ug+x ", 
                 file.path('launch', 'mocaseq', paste0(s_group, '_mocaseq.sh'))))
 }
