6f90d5f654e76ed3f06c50cd6631672e8a55e58a
Marcus Wagner <ga89tog2@cm2login2.cos.lrz.de>
2023-06-01 14:51:20 +0200

    changes to be reverted in merge

--- mocaseq_charliecould.sh
+++ mocaseq_charliecould.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Test script to run MoCaSeq in charliecloud using single SLURM job
-
 #SBATCH -J RAMP_0008-test
 #SBATCH -o ./%x.%j.%N.out
 #SBATCH -D ./
@@ -12,16 +11,12 @@
 #SBATCH --mail-user=marcus.wagner@tum.de
 #SBATCH --export=NONE
 #SBATCH --time=08:00:00
-
 module load slurm_setup
 # module load ...
-
 # modify SLURM cluster to run on
 export SLURM\_CLUSTERS="cm2"
-
 # set container path
 ccc_path=${HOME}/images-live/mocaseq2/
-
 # specify mount path
 working_directory=/gpfs/scratch/pn29ya/ga89tog2/mocaseq-slurm
 ref_directory=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/reference_bash
@@ -29,11 +24,9 @@ script_directory=/dss/dsshome1/0F/ga89tog2/.nextflow/assets/roland-rad-lab/MoCaS
 # TODO: asseble full bam path in pipeline: RAMP_0008_Pa_P/results/bam_remap/
 bam_path_prefix=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh38.p12_bam/batch01/
 # TODO out dir
-
 # specify samples
 sampleT=RAMP_0008_Pa_P
 sampleN=RAMP_0008_Mu_R
-
 # MoCaSeq call inside charliecloud container
 # --bind ${working_directory}:/var/pipeline/ 
 # charliecloud does not allow to bind into existing bind mounts.
