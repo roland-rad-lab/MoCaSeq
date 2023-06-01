18b3af6b020ad9a865c85465540a8e3e7a762125
Marcus Wagner <marcus.wagner@tum.de>
2023-06-01 11:39:53 +0200

    debugging LRZ cm2 partition

--- mocaseq_charliecould.sh
+++ mocaseq_charliecould.sh
@@ -4,8 +4,8 @@
 #SBATCH -J RAMP_0008-test
 #SBATCH -o ./%x.%j.%N.out
 #SBATCH -D ./
-#SBATCH --clusters=cm2_tiny
-#SBATCH --partition=cm2_tiny
+#SBATCH --clusters=cm2
+#SBATCH --partition=cm2_std
 #SBATCH --get-user-env
 #SBATCH --mail-type=end
 #SBATCH --mem=40gb
