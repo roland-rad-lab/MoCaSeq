ab47157ae93aac98fe946ea12ec2af68e83b35e9
Marcus Wagner <marcus.wagner@tum.de>
2023-06-01 17:44:21 +0200

    preparing ccc test for MoCaSeq analysis

--- ccc_mocaseq.sh
+++ ccc_mocaseq.sh
@@ -2,11 +2,11 @@
 # SLURM template for MoCaSeq in charliecloud container (aka ccc)
 # @author: marcus.wagner@tum.de
 
-#SBATCH -J RAMP_0008-test
+#SBATCH -J analysis-test
 #SBATCH -o ./%x.%j.%N.out
 #SBATCH -D ./
 #SBATCH --clusters=cm2
-#SBATCH --partition=cm2_tiny
+#SBATCH --partition=cm2_std
 #SBATCH --get-user-env
 #SBATCH --mail-type=end
 #SBATCH --mem=40gb
@@ -24,23 +24,25 @@ ccc_path=${HOME}/images-live/mocaseq2
 working_directory=/gpfs/scratch/pn29ya/ga89tog2/mocaseq-slurm
 # ref_directory=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/reference_bash # copied reference dir to ${working_directory}/ref
 script_directory=/dss/dsshome1/0F/ga89tog2/.nextflow/assets/roland-rad-lab/MoCaSeq
-bam_path_prefix=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh38.p12_bam/batch01/
+# /dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh38.p12_bam/batch01/
+bam_path_prefix=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/ega_download/debug_bams/bam_remap
 # TODO out dir
 
 # specify samples
-sampleT=RAMP_0008_Pa_P
-sampleN=RAMP_0008_Mu_R
+sampleT=PCSI_0357_Pa_P_5262_1percent
+sampleN=PCSI_0357_St_R_1percent
 
 
 # MoCaSeq call inside charliecloud container
+# TODO: adapt MoCaSeq paths & parameters to sample call
 ch-run $ccc_path --no-home --set-env=sampleT=${sampleT} --set-env=sampleT=${sampleN} -w --no-passwd \
 --bind ${working_directory}:/var/pipeline/ \
 --bind ${script_directory}:/opt/MoCaSeq/ \
 --bind ${bam_path_prefix}:/var/raw-bams/ \
 -- \
 /opt/MoCaSeq/MoCaSeq_COMPASS_WGS.sh \
--tb /var/raw-bams/${sampleT}/results/bam_remap/${sampleT}.Tumor.bam \
--nb /var/raw-bams/${sampleN}/results/bam_remap/${sampleN}.Normal.bam \
+-tb /var/raw-bams/${sampleT}/${sampleT}.Tumor.bam \
+-nb /var/raw-bams/${sampleN}/${sampleN}.Normal.bam \
 --name ${sampleT} \
 --species Human \
 --repeat_mapping no \
