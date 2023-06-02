1eacecafd4d61b49f5af32d02d050d2be2670f89
Marcus Wagner <marcus.wagner@tum.de>
2023-06-02 11:09:40 +0200

    nextfloe sbatch script

---
+++ ccc_nextflow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SLURM template for MoCaSeq in charliecloud container (aka ccc)
+# @author: marcus.wagner@tum.de
+
+#SBATCH -J analysis-test
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -D ./
+#SBATCH --clusters=cm2_tiny
+#SBATCH --nodes=1
+#SBATCH --get-user-env
+#SBATCH --mail-type=end
+#SBATCH --mem=40gb
+#SBATCH --mail-user=marcus.wagner@tum.de
+#SBATCH --export=NONE
+#SBATCH --time=240:00:00
+
+module load slurm_setup
+
+# specify mount path
+working_directory=/gpfs/scratch/pn29ya/ga89tog2/ga89tog2/test/
+
+# nextflow call with charliecloud profile 
+nextflow run main_new.nf -profile charliecloud -work-dir ${working_directory} --custom_config_base ./conf --custom_config_version mocaseq-lrz
+
+
