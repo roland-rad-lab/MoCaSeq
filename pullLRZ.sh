c5a351b7036d2b5daca1e9cdb97b56cd064f2d44
Marcus Wagner <marcus.wagner@tum.de>
2023-06-12 16:19:38 +0200

    debugging remapping launcher scripts

--- pullLRZ.sh
+++ pullLRZ.sh
@@ -10,9 +10,11 @@ chmod 775 MoCaSeq.sh MoCaSeq_LRZ_remap.sh MoCaSeq_LRZ_calling.sh
 # set permissions for files in repository
 chmod 775 repository/all_RunBubbleTree.R repository/SNV_SelectOutput.R repository/SNV_Signatures.R repository/CNV_PlotCNVKit.R repository/CNV_PlotHMMCopy.R repository/SV_MantaPostprocessing.sh repository/CNV_PlotCopywriter.R repository/LOH_Library.R repository/LOH_GenerateVariantTable.R repository/CNV_EstimateCoverage.R repository/CheckReferenceFiles.sh repository/CNV_RunCopywriter.R repository/Preparation_GenerateBWAIndex.sh repository/all_MoCaSeq_lcWGS.sh repository/vcf_annotation/get_usable_indels_for_annoatation.py repository/vcf_annotation/merge_annotation_files.py repository/vcf_annotation/export_variant_input_to_bed.py repository/vcf_annotation/export_repeatmasker2bed.py repository/vcf_annotation/parse_extended_mpileup.py repository/vcf_annotation/filter_variant_input.py repository/vcf_annotation/parse_mpileup.py repository/Preparation_GetExemplaryData.sh repository/LOH_MakePlots.R repository/CNV_RunHMMCopy.sh repository/SNV_SelectOutputSS.R
 
-# set permissions for launch files
+# set permissions for remap launcher files
 chmod 775 launch/ccc_remap_wrapper.sh
 chmod 775 launch/remap/*.sh
-chmod 775 launch/mocaseq/*.sh
-chmod 775 launch/remap/batch0[23]-[A-Z]{4,5}_[0-9]{4}/*.sh
+chmod 775 launch/remap/batch0[2-3]-[A-Z][A-Z][A-Z][A-Z]*_[0-9][0-9][0-9][0-9]/*.sh
+
+# set permissions for MoCaSeq launcher files
+# chmod 775 launch/mocaseq/*.sh
 
