f121f286bc5d6fe188c5bc3060ec28ae71d430f0
Marcus Wagner <marcus.wagner@tum.de>
2023-06-27 12:51:34 +0200

    preparing run for RAMP_0008

---
+++ launch/mocaseq/batch01-RAMP_0008/batch01-RAMP_0008_nf.sh
@@ -0,0 +1,13 @@
+# MoCaSeq launch script for sample groupbatch01-RAMP_0008
+# path to DSS project folder
+projectDir=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS
+# path to MoCaSeq git repo
+repoDir=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS/software/MoCaSeq
+# path to working dir
+workDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/mocaseq
+mkdir -p $workDir
+# nextflow needs this variable to get SLURM job status
+export SLURM\_CLUSTERS="serial"
+
+nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch01-RAMP_0008.tsv
+
