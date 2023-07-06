67f3dbd9e2947ed617a58bc6545b0349d3795aad
Marcus Wagner <marcus.wagner@tum.de>
2023-07-06 11:47:54 +0200

    PCSI_0665 took more than 48h for Base recalibration on cm3 using 44GB ram -> use more ram per sample

--- launch/remap/batch03-PCSI_0665/batch03-PCSI_0665_Lv_M_526_cm3.sh
+++ launch/remap/batch03-PCSI_0665/batch03-PCSI_0665_Lv_M_526_cm3.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#SBATCH -J PCSI_0665_Lv_M_5262-remap
-#SBATCH -o ./%x.%j.%N.out
+#SBATCH -J batch03-PCSI_0665_Lv_M_526-remap
+#SBATCH -o ./PCSI_0665_Lv_M_526.%j.%N.out
 #SBATCH -D ./
 #SBATCH --clusters=mpp3
 #SBATCH --partition=mpp3_batch
 #SBATCH --nodes=1
 #SBATCH --time=48:00:00
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=64
 #SBATCH --mem=91136mb
 #SBATCH --get-user-env
 #SBATCH --mail-type=end
@@ -38,12 +36,11 @@ ln -s $referencesDir ref
 mv ref $workingDir
 
 # specify sample
-sample=PCSI_0665_Lv_M_5262
-bamDir=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh37_bam/batch03/EGAF00002251670
+sample=PCSI_0665_Lv_M_526
+bamDir=/dss/dssfs02/lwp-dss-0001/pn29ya/pn29ya-dss-0000/projects/hPDAC/ICGC_PACA_CA_WGS/input/GRCh37_bam//batch03/EGAF00002251670
 bamName=PCSI_0665_Lv_M_5262.bam
 bamType="Tumor"
 # submit subjob for sample remapping
-srun --ntasks=1 --exclusive --mem 91136mb -J $sample -o ./%x.%j.%N.out ${mocaseqDir}/launch/ccc_remap_wrapper.sh -ccc $cccDir -wd $workingDir -m $mocaseqDir -bd $bamDir -bf $bamName -s $sample -rd $referencesDir -t $bamType -r 89 -@ 64 > ${sample}-remap.out & 
-sleep 4
+${mocaseqDir}/launch/ccc_remap_wrapper.sh -ccc $cccDir -wd $workingDir -m $mocaseqDir -bd $bamDir -bf $bamName -s $sample -rd $referencesDir -t $bamType -r 89 -@ 64 &
 
 wait # for completion of background tasks
