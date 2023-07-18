83f38d767249c9f5cebcf7fdb55d75278d8f3413
Marcus Wagner <marcus.wagner@tum.de>
2023-07-18 11:32:10 +0200

    removed bug in calling SNV_Mutect2Postprocessing_LRZ.sh

--- launch/ccc_Mutect_Postprocessing.sh
+++ launch/ccc_Mutect_Postprocessing.sh
@@ -7,8 +7,10 @@ GATK=4.1.7.0
 type=matched
 repository_dir=/opt/MoCaSeq/repository
 
+. $config_file
+
 bash $repository_dir/SNV_Mutect2Postprocessing_LRZ.sh $name $species $config_file \
-$name $species $config_file $filtering $artefact_type $GATK $type
+ $filtering $artefact_type $GATK $type
 
 Rscript $repository_dir/SNV_SelectOutput.R $name Mutect2 $species $CGC_file $TruSight_file
 
