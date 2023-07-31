aa51a658835eab8b02ad5540623bf1f29f64e059
Marcus Wagner <marcus.wagner@tum.de>
2023-07-31 10:49:21 +0200

    PCSI_0683 remapping

--- launch/remap/batch02-PCSI_0683/batch02-PCSI_0683_nf.sh
+++ launch/remap/batch02-PCSI_0683/batch02-PCSI_0683_nf.sh
@@ -9,5 +9,5 @@ workingDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/remap
 
 
 # nextflow call with charliecloud profile 
-nextflow run ${mocaseqDir}/main.nf -profile charliecloud -entry MAP -work-dir ${workingDir} --output_base ${projectDir}/input --genome_build.human GRCh38.p12 --custom_config_base ${mocaseqDir}/conf --custom_config_version serial-std --input batch02-PCSI_0683.tsv -resume
+nextflow run ${mocaseqDir}/main.nf -profile charliecloud,slurm -entry MAP -work-dir ${workingDir} --output_base ${projectDir}/input --genome_build.human GRCh38.p12 --custom_config_base ${mocaseqDir}/conf --custom_config_version serial-std --input batch02-PCSI_0683.tsv -resume
 
