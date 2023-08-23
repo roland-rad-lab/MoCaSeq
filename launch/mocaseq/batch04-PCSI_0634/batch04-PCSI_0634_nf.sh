2e0b171767bad96d06ef6856fd9da70c8ebef8c0
Marcus Wagner <marcus.wagner@tum.de>
2023-08-23 10:42:36 +0200

    updated batch04 launchers to run on mpp3

--- launch/mocaseq/batch04-PCSI_0634/batch04-PCSI_0634_nf.sh
+++ launch/mocaseq/batch04-PCSI_0634/batch04-PCSI_0634_nf.sh
@@ -8,4 +8,4 @@ workDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/mocaseq
  # nextflow needs this variable to get SLURM job status
 export SLURM\_CLUSTERS="serial"
  
-nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch04-PCSI_0634.tsv
+nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version mpp3-batch --custom_config_base ${repoDir}/conf --input batch04-PCSI_0634.tsv
