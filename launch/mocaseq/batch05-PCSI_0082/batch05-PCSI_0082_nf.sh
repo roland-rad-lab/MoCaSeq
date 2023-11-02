3dca73d9867722c6f68607b5b0ff54d5745d06fc
Marcus Wagner <marcus.wagner@tum.de>
2023-11-02 17:58:00 +0100

    mocaseq lauchers

--- launch/mocaseq/batch05-PCSI_0082/batch05-PCSI_0082_nf.sh
+++ launch/mocaseq/batch05-PCSI_0082/batch05-PCSI_0082_nf.sh
@@ -1,4 +1,4 @@
-# MoCaSeq launch script for sample groupbatch06-PCSI_0673
+# MoCaSeq launch script for sample groupbatch05-PCSI_0082
  # path to DSS project folder
 projectDir=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS
  # path to MoCaSeq git repo
@@ -8,4 +8,4 @@ workDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/mocaseq
  # nextflow needs this variable to get SLURM job status
 export SLURM\_CLUSTERS="serial"
  
-nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch06-PCSI_0673.tsv -N marcus@mpwagner.net
+nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch05-PCSI_0082.tsv -N marcus@mpwagner.net
--- launch/mocaseq/batch05-PCSI_0083/batch05-PCSI_0083_nf.sh
+++ launch/mocaseq/batch05-PCSI_0083/batch05-PCSI_0083_nf.sh
@@ -8,4 +8,4 @@ workDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/mocaseq
  # nextflow needs this variable to get SLURM job status
 export SLURM\_CLUSTERS="serial"
  
-nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch05-PCSI_0083.tsv
+nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch05-PCSI_0083.tsv -N marcus@mpwagner.net
