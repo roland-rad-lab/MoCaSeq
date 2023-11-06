3963f3d12a8784c0fb40a18a2758e6ad6828b916
Marcus Wagner <marcus.wagner@tum.de>
2023-11-06 16:29:09 +0100

    remap and mocaseq lauchers for batch5 samples

---
+++ launch/remap/batch05-PCSI_0264/batch05-PCSI_0264_nf.sh
@@ -0,0 +1,4 @@
+# MoCaSeq launch script for sample group batch05-PCSI_0264
+ projectDir=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS
+ workDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/remap
+ nextflow run /dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS/software/MoCaSeq/main.nf -profile charliecloud,slurm -entry MAP -with-report -with-timeline -work-dir ${workDir}/remap/work --output_base ${projectDir}/input --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base /dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS/software/MoCaSeq/conf --input batch05-PCSI_0264.tsv -N marcus.wagner@tum.de
