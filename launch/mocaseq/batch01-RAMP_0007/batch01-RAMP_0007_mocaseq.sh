63fe9e45bf7308e63269009e98e1a364b530e92d
Marcus Wagner <marcus.wagner@tum.de>
2024-02-20 13:15:16 +0100

    adapting MoCaSeq runner for RAMP_0007_Pa_P

--- launch/mocaseq/batch01-RAMP_0007/batch01-RAMP_0007_mocaseq.sh
+++ launch/mocaseq/batch01-RAMP_0007/batch01-RAMP_0007_mocaseq.sh
@@ -1,7 +1,12 @@
-# MoCaSeq launch script for sample groupbatch01-RAMP_0007
-# new DSS path for project specific long ther storage (save results here)
-export DSS2=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS
-# git repository path
-export REPO=${DSS2}/software/MoCaSeq
-workDir=/gpfs/scratch/pn29ya/ga89tog2/mocaseq-nextflow/mocaseq
-nextflow run ${REPO}/main.nf -profile charliecloud,slurm -work-dir ${workDir}/mocaseq/work --output_base ${DSS2}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${REPO}/conf --input ${REPO}/input/mocaseq/batch01-RAMP_0007.tsv -resume
+# MoCaSeq launch script for sample groupbatch05-PCSI_0735
+ # path to DSS project folder
+projectDir=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS
+ # path to MoCaSeq git repo
+repoDir=/dss/dssfs03/tumdss/pn72lo/pn72lo-dss-0006/projects/hPDAC/ICGC_PACA_CA_WGS/software/MoCaSeq
+ # path to working dir
+workDir=/gpfs/scratch/pn29ya/$USER/mocaseq-nextflow/mocaseq
+ # nextflow needs this variable to get SLURM job status
+export SLURM\_CLUSTERS="serial"
+ 
+nextflow run ${repoDir}/main.nf -profile charliecloud,slurm -with-report -with-timeline -work-dir ${workDir} --output_base ${projectDir}/output --genome_build.human GRCh38.p12 --custom_config_version serial-std --custom_config_base ${repoDir}/conf --input batch01-RAMP_0007.tsv
+
